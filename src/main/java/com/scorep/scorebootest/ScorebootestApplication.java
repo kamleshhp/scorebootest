6513db78b2e6cdc9e6b339958a88497ddff9ff25
ksharma04 <kamlesh.sharma@factset.com>
2022-12-27 15:40:59 +0530

    initial commit

---
+++ src/main/java/com/scorep/scorebootest/ScorebootestApplication.java
@@ -0,0 +1,13 @@
+package com.scorep.scorebootest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScorebootestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScorebootestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scorep/scorebootest/ScorebootestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scorep.scorebootest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScorebootestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
